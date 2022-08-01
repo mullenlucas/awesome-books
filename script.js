435ad62cfb7858576d8e3243b394bb1436b281b1
mullenslucas <mullenlucas@gmail.com>
2022-08-01 18:24:48 -0300

    add books to list

--- script.js
+++ script.js
@@ -7,49 +7,63 @@ let books = [
    titulo: 'El tirano',
    author: 'Massimo Manfredy'
     }
-
 ]
 
 const bookList = document.querySelector('.books-display');
 let displ = ''
 books.forEach((el, index) => {    
-    displ += `<h4> ${el.titulo} </h4> <h4> ${el.author} </h4>  <button class="btn-display">remove</button>  ` 
-    
+    displ += `
+    <div class="oneBook" id="book${index}">
+    <h4> ${el.titulo} </h4>
+    <h4> ${el.author} </h4> 
+    <button class="btn-display" id="remove-id${index}">remove</button> 
+    </div>
+    ` 
+
 });
 bookList.innerHTML = displ
-
-
-
-/* const add = function (){    
-    const reciTi = document.getElementById("recibe-ti").value
-    const reciAu = document.getElementById("recibe-au").value
-   // console.log(reciAu);
-let newBook = {
-    titulo: reciTi,
-    author: reciAu
-}
-    books.push(newBook)
-} */
+let removIds = [...document.querySelectorAll('.btn-display')].map(item => item.id)
 
 const newB = document.getElementById('bk')
 
-newB.addEventListener('click', function(event){
+newB.addEventListener('submit', function(event){
+
     event.preventDefault();
     const reciTi = document.getElementById("recibe-ti").value
     const reciAu = document.getElementById("recibe-au").value
-   // console.log(reciAu);
-   if(reciTi !== ''&& reciAu !== '' ){
-let newBook = {
-    titulo: reciTi,
-    author: reciAu
-}
-    books.push(newBook)
-}
 
+    if(reciTi !== '' && reciAu !== ''){
+    let newBook = {
+        titulo: reciTi,
+        author: reciAu
+    }
+    books.unshift(newBook)
 
+    let displ = ''
+    books.forEach((el, index) => {    
+    displ += `
+    <div class="oneBook" id="book${index}">
+    <h4> ${el.titulo} </h4>
+    <h4> ${el.author} </h4> 
+    <button class="btn-display" id="remove-id${index}">remove</button> 
+    </div>
+    `
 
-   })
+    });
+    bookList.innerHTML = displ
+
+    console.log(books)
+    let removIds = [...document.querySelectorAll('.btn-display')].map(item => item.id)
+    }
+})
+
+document.querySelectorAll('.btn-display').forEach( (rem) => {
+    rem.addEventListener('click', (event) => {
+        console.log(rem)
+    })
+
+})
+
 
 
 console.log(books);
-console.log('hola');
