fc78be8f202c4bb6e9072f2bf0ebb1052a9014b2
mullenslucas <mullenlucas@gmail.com>
2022-08-02 12:34:48 -0300

    fix linter errors and erase unused .css file

--- script.js
+++ script.js
@@ -1,75 +1,69 @@
 let books = [
-    {
+  {
     titulo: '100 años de soledad',
-    author: 'Gabriel Garcia Marquez'
-    },
-    {
+    author: 'Gabriel Garcia Marquez',
+  },
+  {
     titulo: 'El tirano',
-    author: 'Massimo Manfredi'
-    }
-]
+    author: 'Massimo Manfredi',
+  },
+];
 
-function storage(ok){
-window.localStorage.setItem('localBooks', JSON.stringify(ok))
-
+function storage(ok) {
+  window.localStorage.setItem('localBooks', JSON.stringify(ok));
 }
 
-function getBooks(){
-    books = JSON.parse(window.localStorage.getItem('localBooks'))
+function getBooks() {
+  books = JSON.parse(window.localStorage.getItem('localBooks'));
 }
 
-getBooks()
+getBooks();
 
-function display(){
-const bookList = document.querySelector('.books-display');
-let displ = ''
-books.forEach((el, index) => {    
+function display() {
+  const bookList = document.querySelector('.books-display');
+  let displ = '';
+  books.forEach((el, index) => {
     displ += `
     <div class="oneBook" id="book${index}">
     <h4> ${el.titulo} </h4>
     <h4> ${el.author} </h4> 
     <button class="btn-display" id="${index}">remove</button> 
     </div>
-    ` 
-
-});
-bookList.innerHTML = displ
+    `;
+  });
+  bookList.innerHTML = displ;
 }
 
-// ---------ADD------------- 
+// ---------ADD-------------
 
-const newB = document.getElementById('bk')
-newB.addEventListener('submit', function(event){
+const newB = document.getElementById('bk');
+newB.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const reciTi = document.getElementById('recibe-ti').value;
+  const reciAu = document.getElementById('recibe-au').value;
 
-    event.preventDefault();
-    const reciTi = document.getElementById("recibe-ti").value
-    const reciAu = document.getElementById("recibe-au").value
-
-    if(reciTi !== '' && reciAu !== ''){
-    let newBook = {
-        titulo: reciTi,
-        author: reciAu
-    }
-   books.unshift(newBook)
-  storage(books)
-  display()
-  
-    }
-})
+  if (reciTi !== '' && reciAu !== '') {
+    const newBook = {
+      titulo: reciTi,
+      author: reciAu,
+    };
+    books.unshift(newBook);
+    storage(books);
+    display();
+  }
+});
 
 // -------DELETE----------
-function deleteBook(evento){
-    books = books.filter((el, index) =>
-    evento.target.id !== index.toString()
-    )
-   storage(books)
-   display()
+function deleteBook(evento) {
+  books = books.filter((el, index) => evento.target.id !== index.toString());
+  storage(books);
+  display();
 }
 
-document.addEventListener('click', e => {
-    if(e.target.matches('.btn-display') ){
-       deleteBook(e)
-}
-})
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.btn-display')) {
+    deleteBook(e);
+  }
+});
 
-display()
+display();
