50748809bee2733283176beb97109b55950837bf
mullenslucas <mullenlucas@gmail.com>
2022-08-03 13:29:47 -0300

    add date and adapt footer

--- script.js
+++ script.js
@@ -118,3 +118,12 @@ contactBtn.addEventListener('click',(e)=>{
 } )
 
 
+let today = new Date()
+let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+let dateTime = date+' '+time;
+
+const dateId = document.getElementById('date-time-id')
+
+dateId.innerHTML = dateTime
